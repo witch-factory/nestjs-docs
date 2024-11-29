b8ef900ddfbf8dba996e7c4392e7d271a21ebc8b
witch-factory <soakdma37@gmail.com>
2024-11-29 14:30:33 +0900

    사용자 role을 이용한 가드 구현

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -5,13 +5,16 @@ import {
   Param,
   ParseIntPipe,
   Post,
-  UsePipes,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { Roles } from 'src/roles/roles.decorator';
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
   // injected
   // catsService 선언과 초기화
@@ -19,6 +22,7 @@ export class CatsController {
 
   // 이때 Postman으로 요청 보내면 age가 string이라서 에러 발생
   @Post()
+  @Roles(['admin'])
   create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     console.log(createCatDto);
     this.catsService.create(createCatDto);
